a73776683551ff38c0c2913eee9ba084d038007e
Przemek Anuszek <przemas@anuszek.net>
2019-10-14 19:39:01 +0200

    aaa

--- spec/classes/chrony_spec.rb
+++ spec/classes/chrony_spec.rb
@@ -133,7 +133,7 @@ describe 'chrony' do
               it { is_expected.to contain_file('/etc/chrony/chrony.keys').with_group('mrt') }
               it { is_expected.to contain_file('/etc/chrony/chrony.keys').with_replace(true) }
               it { is_expected.to contain_file('/etc/chrony/chrony.keys').with_content("0 sunny\n") }
-              let facts[:operatingsystemmajrelease] > '9'
+              case facts[:operatingsystemmajrelease] > '8'
                 it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*leapsecmode slew$}) }
                 it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*maxslewrate 1000$}) }
                 it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*smoothtime 400 0\.001 leaponly$}) }
