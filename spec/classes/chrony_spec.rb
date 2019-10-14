e96555b862f199d31bf1904a29806df32044906b
Przemek Anuszek <przemas@anuszek.net>
2019-10-14 20:10:36 +0200

    float 1

--- spec/classes/chrony_spec.rb
+++ spec/classes/chrony_spec.rb
@@ -84,7 +84,7 @@ describe 'chrony' do
             bindcmdaddress: ['10.0.0.1'],
             cmdacl: ['cmdallow 1.2.3.4', 'cmddeny 1.2.3', 'cmdallow all 1.2'],
             leapsecmode: 'slew',
-            maxslewrate: 1000,
+            maxslewrate: 1000.00,
             smoothtime: '400 0.001',
           }
         end
