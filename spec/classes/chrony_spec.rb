e0966acc67dddc7713d55ff6eafdb402de6cf081
Przemek Anuszek <przemas@anuszek.net>
2019-10-15 15:02:11 +0200

    sdfsdfgdfhdfgh

--- spec/classes/chrony_spec.rb
+++ spec/classes/chrony_spec.rb
@@ -83,11 +83,10 @@ describe 'chrony' do
             chrony_password: 'sunny',
             bindcmdaddress: ['10.0.0.1'],
             cmdacl: ['cmdallow 1.2.3.4', 'cmddeny 1.2.3', 'cmdallow all 1.2'],
-            if facts[:operatingsystemmajrelease] > '8'
-              leapsecmode: 'slew',
-              maxslewrate: 1000.0,
-              smoothtime: '400 0.001 leaponly',
-            end
+            # if facts[:operatingsystemmajrelease] > '8'
+            #leapsecmode: 'slew',
+            #maxslewrate: 1000.0,
+            #smoothtime: '400 0.001 leaponly',
           }
         end
 
@@ -138,11 +137,9 @@ describe 'chrony' do
               it { is_expected.to contain_file('/etc/chrony/chrony.keys').with_group('mrt') }
               it { is_expected.to contain_file('/etc/chrony/chrony.keys').with_replace(true) }
               it { is_expected.to contain_file('/etc/chrony/chrony.keys').with_content("0 sunny\n") }
-              if facts[:operatingsystemmajrelease] > '8'
-                it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*leapsecmode slew$}) }
-                it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*maxslewrate 1000\.0$}) }
-                it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*smoothtime 400 0\.001 leaponly$}) }
-              end
+              it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*leapsecmode slew$}) }
+              it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*maxslewrate 1000\.0$}) }
+              it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*smoothtime 400 0\.001 leaponly$}) }
             end
           end
         end
